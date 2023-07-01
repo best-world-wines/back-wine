ee44df2a4e03edcd7bfb213f3ace276228af74af
iskamele <iskamele@gmail.com>
2023-07-01 05:20:35 +0300

    Removed extra images-model classes.

--- src/main/java/api/backwine/repository/RegionImageRepository.java
+++
@@ -1,9 +0,0 @@
-package api.backwine.repository;
-
-import api.backwine.model.RegionImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegionImageRepository extends JpaRepository<RegionImage, Long> {
-}
