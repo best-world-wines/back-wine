f656b50bd394f888f001a034090e22e78975f121
iskamele <iskamele@gmail.com>
2023-07-03 16:02:23 +0300

    Removed Winery model.

--- src/main/java/api/backwine/repository/WineryRepository.java
+++
@@ -1,9 +0,0 @@
-package api.backwine.repository;
-
-import api.backwine.model.Winery;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WineryRepository extends JpaRepository<Winery, Long> {
-}
