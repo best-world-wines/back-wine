c8f40ecd6bd822fd226cbc9719e2b5df49faec26
iskamele <iskamele@gmail.com>
2023-07-07 01:14:19 +0300

    Added new methods, implemented abstract service.

--- src/main/java/api/backwine/controller/WineController.java
+++ src/main/java/api/backwine/controller/WineController.java
@@ -4,56 +4,65 @@ import api.backwine.dto.mapper.WineMapper;
 import api.backwine.dto.request.WineRequestDto;
 import api.backwine.dto.response.WineResponseDto;
 import api.backwine.model.Wine;
-import api.backwine.service.WineService;
+import api.backwine.service.AbstractService;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/wines")
 public class WineController {
-
-    private final WineService wineService;
+    private final AbstractService<Wine> service;
     private final WineMapper wineMapper;
 
-    public WineController(WineService wineService, WineMapper wineMapper) {
-        this.wineService = wineService;
-        this.wineMapper = wineMapper;
+    public WineController(AbstractService<Wine> service, WineMapper wineryMapper) {
+        this.service = service;
+        this.wineMapper = wineryMapper;
     }
 
-    @PostMapping("/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public WineResponseDto createWine(@Valid @RequestBody WineRequestDto wineRequestDto) {
-        return wineMapper.mapToDto(wineService.create(wineMapper.mapToModel(wineRequestDto)));
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping()
+    public ResponseEntity<WineResponseDto> create(@Valid @RequestBody WineRequestDto wineRequestDto) {
+        return new ResponseEntity<>(wineMapper.toDto(
+                service.create(wineMapper.toModel(wineRequestDto))), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<WineResponseDto> getAll() {
-        return wineService.getAll()
+    public ResponseEntity<List<WineResponseDto>> getAll() {
+        return ResponseEntity.ok(service.getAll()
                 .stream()
-                .map(wineMapper::mapToDto)
-                .collect(Collectors.toList());
+                .map(wineMapper::toDto)
+                .collect(Collectors.toList()));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
-    public WineResponseDto updateWine(@PathVariable Long id,
-                                      @Valid @RequestBody WineRequestDto wineRequestDto) {
-        Wine wine = wineMapper.mapToModel(wineRequestDto);
-        wine.setId(id);
-        return wineMapper.mapToDto(wineService.create(wine));
+    public ResponseEntity<WineResponseDto> update(@PathVariable("id") Long id,
+                                                  @Valid @RequestBody WineRequestDto wineRequestDto) {
+        return ResponseEntity.ok(wineMapper.toDto(
+                service.update(id, wineMapper.toModel(wineRequestDto))));
     }
 
     @GetMapping("/{id}")
-    public WineResponseDto getWine(@PathVariable Long id) {
-        return wineMapper.mapToDto(wineService.getById(id));
+    public ResponseEntity<WineResponseDto> get(@PathVariable Long id) {
+        return ResponseEntity.ok(wineMapper.toDto(service.getById(id)));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") Long id) {
+        service.deleteById(id);
+        return ResponseEntity.ok("Success, deleted entity by id " + id);
     }
 }
