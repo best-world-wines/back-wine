c1475469199355ea5423d0dae40c748b4f9b42e4
iskamele <iskamele@gmail.com>
2023-07-12 00:41:37 +0300

    Fixed country field. Added relation and joinTable.

--- src/main/java/api/backwine/model/Region.java
+++ src/main/java/api/backwine/model/Region.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,8 +25,13 @@ public class Region {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Column(name = "country_name")
-    private String countryName;
+    @ManyToOne
+    @JoinTable(
+            name = "country_regions",
+            joinColumns = @JoinColumn(name = "region_id"),
+            inverseJoinColumns = @JoinColumn(name = "country_id")
+    )
+    private Country country;
     private String backgroundImage;
     @Column(name = "is_deleted")
     private boolean isDeleted;
