589370f061b1256bf8adbeee3a25972d2737ebe6
iskamele <iskamele@gmail.com>
2023-07-12 00:38:24 +0300

    Removed regions field.

--- src/main/java/api/backwine/dto/response/CountryResponseDto.java
+++ src/main/java/api/backwine/dto/response/CountryResponseDto.java
@@ -1,14 +1,12 @@
 package api.backwine.dto.response;
 
 import api.backwine.model.Grape;
-import api.backwine.model.Region;
-import java.util.Set;
+import java.util.List;
 import lombok.Data;
 
 @Data
 public class CountryResponseDto {
     private String code;
     private String name;
-    private Set<Region> regions;
-    private Set<Grape> grapes;
+    private List<Grape> mostUsedGrapes;
 }
