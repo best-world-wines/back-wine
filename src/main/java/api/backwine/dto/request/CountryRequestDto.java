21ec4dd026b2870979b6d1c6a7759b5f5337b220
iskamele <iskamele@gmail.com>
2023-07-11 18:53:14 +0300

    Added code field.

--- src/main/java/api/backwine/dto/request/CountryRequestDto.java
+++ src/main/java/api/backwine/dto/request/CountryRequestDto.java
@@ -7,6 +7,8 @@ import lombok.Data;
 @Data
 public class CountryRequestDto {
     @NotNull
+    private String code;
+    @NotNull
     private String name;
     @NotNull
     private List<Long> regionsIds;
