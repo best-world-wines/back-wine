1498f597c6d4b7eb292db3c93c28aaadeae0bb56
iskamele <iskamele@gmail.com>
2023-07-27 14:47:59 +0300

    Changed code type. Implemented interface.

--- src/main/java/api/backwine/dto/mapper/product/CountryMapper.java
+++ src/main/java/api/backwine/dto/mapper/product/CountryMapper.java
@@ -1,18 +1,27 @@
-package api.backwine.dto.mapper;
+package api.backwine.dto.mapper.product;
 
-import api.backwine.dto.request.CountryRequestDto;
-import api.backwine.dto.response.CountryResponseDto;
-import api.backwine.model.Country;
-import api.backwine.model.Grape;
+import api.backwine.dto.mapper.GlobalMapper;
+import api.backwine.dto.request.product.CountryRequestDto;
+import api.backwine.dto.response.product.CountryResponseDto;
+import api.backwine.model.product.Country;
+import api.backwine.model.product.CountryCode;
+import api.backwine.model.product.Grape;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CountryMapper {
+public class CountryMapper
+        implements GlobalMapper<Country, CountryRequestDto, CountryResponseDto> {
 
+    @Override
     public Country toModel(CountryRequestDto countryRequestDto) {
         Country country = new Country();
-        country.setCode(countryRequestDto.getCode());
+        try {
+            country.setCode(CountryCode.valueOf(countryRequestDto.getCode()));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Can't find country code for: "
+                    + countryRequestDto.getCode(), e);
+        }
         country.setName(countryRequestDto.getName());
         country.setMostUsedGrapes(countryRequestDto.getMostUsedGrapesIds()
                 .stream()
@@ -25,9 +34,10 @@ public class CountryMapper {
         return country;
     }
 
+    @Override
     public CountryResponseDto toDto(Country country) {
         CountryResponseDto countryResponseDto = new CountryResponseDto();
-        countryResponseDto.setCode(country.getCode());
+        countryResponseDto.setCode(country.getCode().toString());
         countryResponseDto.setName(country.getName());
         countryResponseDto.setMostUsedGrapes(country.getMostUsedGrapes());
         return countryResponseDto;
