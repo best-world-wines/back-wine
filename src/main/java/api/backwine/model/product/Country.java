b4e052bcb99b24f860061b614494d093c099b8a4
iskamele <iskamele@gmail.com>
2023-07-29 19:54:54 +0300

    Moved to new directory. Implemented new interfaces. Added listener.

--- src/main/java/api/backwine/model/product/Country.java
+++ src/main/java/api/backwine/model/product/Country.java
@@ -1,7 +1,10 @@
-package api.backwine.model;
+package api.backwine.model.product;
 
+import api.backwine.model.listener.CountryListener;
+import api.backwine.model.listener.GlobalTimestampedEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -11,18 +14,18 @@ import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "countries")
-@Where(clause = "is_deleted = false")
 @Setter
 @Getter
 @NoArgsConstructor
-public class Country {
+@EntityListeners(CountryListener.class)
+public class Country extends GlobalTimestampedEntity
+        implements GlobalProductIdentifiable<CountryCode> {
     @Id
     @Column(name = "code", nullable = false)
-    private String code;
+    private CountryCode code;
     private String name;
     @ManyToMany
     @JoinTable(
@@ -31,14 +34,22 @@ public class Country {
             inverseJoinColumns = @JoinColumn(name = "grape_id")
     )
     private List<Grape> mostUsedGrapes;
-    @Column(name = "is_deleted")
-    private boolean isDeleted;
 
-    public String getCode() {
+    public CountryCode getCode() {
         return code;
     }
 
-    public void setCode(String code) {
+    public void setCode(CountryCode code) {
         this.code = code;
+    }
+
+    @Override
+    public CountryCode getId() {
+        return null;
+    }
+
+    @Override
+    public void setId(CountryCode id) {
+
     }
 }
