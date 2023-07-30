3cfe7bbddfbd8f954fcf5e2812f9b04a8763c5ee
iskamele <iskamele@gmail.com>
2023-07-30 14:41:52 +0300

    Added abstract class for Instant points for models.

---
+++ src/main/java/api/backwine/model/listener/GlobalTimestampedEntity.java
@@ -0,0 +1,23 @@
+package api.backwine.model.listener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Setter
+@Getter
+@MappedSuperclass
+@EntityListeners({})
+public abstract class GlobalTimestampedEntity {
+    @CreationTimestamp
+    @Column(name = "creating_date", nullable = false, updatable = false)
+    private Instant creatingDate;
+    @Column(name = "updating_date")
+    private Instant updatingDate;
+    @Column(name = "deleting_date")
+    private Instant deletingDate;
+}
